fd4847e77727d365eeaba559acf6625768d9ffdc
Mohammod Ali <mohammodali651@gmail.com>
2024-11-25 23:49:51 +0600

    delete cart function aded

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middle ware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7haskyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,37 +25,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection = client.db("bistroDb").collection("menu")
-    const reviewCollection = client.db("bistroDb").collection("reviews")
-    const cartCollection = client.db("bistroDb").collection("carts")
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
-    app.get('/menu', async(req, res) =>{
-      const result = await menuCollection.find().toArray()
-      res.send(result)
-    })
-    app.get('/reviews', async(req, res) =>{
-      const result = await reviewCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     // carts collection
-    app.get('/carts', async(req, res) =>{
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      const query = { email: email}
-      const result = await cartCollection.find(query).toArray()
-      res.send(result)
-    }) 
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/carts', async(req, res) => {
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
-      const result = await cartCollection.insertOne(cartItem)
-      res.send(result)
-    })
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
 
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -64,11 +71,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('boss is running')
-})
+app.get("/", (req, res) => {
+  res.send("boss is running");
+});
 
 app.listen(port, () => {
-    console.log(`Bistro Boss is running on port ${port}`)
-})
+  console.log(`Bistro Boss is running on port ${port}`);
+});
