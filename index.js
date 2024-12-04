a19bfc80d65cb1148bb384ef3da8ffa84654d387
Mohammod Ali <mohammodali651@gmail.com>
2024-12-04 21:19:39 +0600

    users realted api added

--- index.js
+++ index.js
@@ -31,19 +31,31 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     // users related API
-    app.post('/users', async(req, res) => {
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user doesnt exists:
-      const query = {email: user.email}
-      const existingUser = await userCollection.findOne(query)
-    if(existingUser){
-      return res.send({message: 'user already exists', insertedId: null})
-    }
-      const result = await userCollection.insertOne(user)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
-
+    // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
