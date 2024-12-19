138ef1448b314a76442fb8fdf12cf29018e19a20
Mohammod Ali <mohammodali651@gmail.com>
2024-12-20 00:50:22 +0600

    payment intent added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middle ware
@@ -32,39 +33,41 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     //jst related api
-    app.post('/jwt', async(req, res) => {
-      const user = req.body
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' } )
-      res.send({token})
-    })
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // verify token middlewares
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers)
-      if(!req.headers.authorization){
-        return res.status(401).send({message: 'unauthorized access'})
+      console.log("inside verify token", req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
-      const token = req.headers.authorization.split(' ')[1]
+      const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-          return res.status(401).send({message: 'unauthorized access'})
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
-        next()
-      })
-    }
+        next();
+      });
+    };
 
     // use verify admin after verify token
     const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email
-      const query = {email: email}
-      const user = await userCollection.findOne(query)
-      const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden access'})
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      next()
-    }
+      next();
+    };
 
     // users related API
     app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
@@ -72,19 +75,19 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/user/admin/:email', verifyToken, async(req, res) => {
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      if(email != req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (email != req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const query = {email: email}
-      const user = await userCollection.findOne(query)
-      let admin = false
-      if(user){
-        admin = user?.role === 'admin'
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
       }
-      res.send({admin})
-    })
+      res.send({ admin });
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -98,24 +101,29 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) =>{
-      const id = req.params;
-      const filter = { _id: new ObjectId(id)}
-      const updatedDoc = {
-        $set: {
-          role: 'admin'
-        }
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
       }
-      const result = await userCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
+    );
 
-    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) =>{
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // menu related api
     app.get("/menu", async (req, res) => {
@@ -123,27 +131,26 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/menu/:id', async(req, res) =>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await menuCollection.findOne(query)
-      res.send(result)
-      console.log(id)
-      console.log(query)
-      console.log(result)
-    })
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+      console.log(id);
+      console.log(query);
+      console.log(result);
+    });
 
-   
-    app.post('/menu', verifyToken, verifyAdmin, async(req, res) =>{
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.patch('/menu/:id', async(req, res) => {
-      const item = req.body
-      const id = req.params.id
-      const filter = {_id: new ObjectId(id)}
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           name: item.name,
@@ -151,18 +158,18 @@ async function run() {
           price: item.price,
           recipe: item.recipe,
           image: item.image,
-        }
-      }
-      const result = await menuCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await menuCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
@@ -188,6 +195,22 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // Payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: [card],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     });
 
     // Send a ping to confirm a successful connection
